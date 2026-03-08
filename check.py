663fd669b02d7f8adff00df43b983212cf043b70
rei1237 <seong@example.com>
2026-03-08 09:40:17 +0900

    chore: remove dead utility scripts and cleanup tracked artifacts

--- check.py
+++
@@ -1,5 +0,0 @@
-import re
-with open('index.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-for m in re.finditer(r'<div id="\w+ModalSheet"[^>]+>\s*<div[^>]+padding:(.*?)box-sizing:border-box;', text):
-    print(m.group(0))
